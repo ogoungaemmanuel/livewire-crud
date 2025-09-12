e39922f2c249da3cda44d6426c4bff81c8afd562
ogounga emmanuel <xslainconcept@gmail.com>
2025-09-12 17:20:44 +0100

    upgrade

--- rector.php
+++ rector.php
@@ -3,12 +3,20 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/config',
         __DIR__ . '/src',
-    ])
+    ]) // register single rule
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ]) // here we can define, what prepared sets of rules will be applied
+    // ->withPreparedSets(
+    //     deadCode: true,
+    //     codeQuality: true
+    // )
     // uncomment to reach your current PHP version
      ->withPhpSets()
     ->withTypeCoverageLevel(0)
