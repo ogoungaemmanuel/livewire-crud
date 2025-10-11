1acf5d03f1773f8c47afba51ceae65e1a323483d
ogounga emmanuel <xslainconcept@gmail.com>
2025-10-11 16:14:03 +0100

    Update rector.php

--- rector.php
+++ rector.php
@@ -3,22 +3,92 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
+use Rector\CodeQuality\Rector\Expression\InlineIfToExplicitIfRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
+use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
+use Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
+use Rector\Laravel\Set\LaravelSetList;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/config',
         __DIR__ . '/src',
-    ]) // register single rule
+        __DIR__ . '/tests',
+    ])
+    ->withSkip([
+        // Skip stub files as they are templates
+        __DIR__ . '/src/stubs',
+        __DIR__ . '/resources',
+        
+        // Skip specific rules that might break Laravel patterns
+        UnSpreadOperatorRector::class,
+        EncapsedStringsToSprintfRector::class,
+        InlineIfToExplicitIfRector::class,
+    ])
+    ->withPhpSets(
+        php81: true
+    )
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true
+    )
+    ->withSets([
+        LaravelSetList::LARAVEL_100,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+        LevelSetList::UP_TO_PHP_81,
+    ])
     ->withRules([
-        TypedPropertyFromStrictConstructorRector::class
-    ]) // here we can define, what prepared sets of rules will be applied
-    // ->withPreparedSets(
-    //     deadCode: true,
-    //     codeQuality: true
-    // )
-    // uncomment to reach your current PHP version
-     ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+        // Type declarations
+        TypedPropertyFromStrictConstructorRector::class,
+        TypedPropertyRector::class,
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        ReturnTypeFromStrictTypedPropertyRector::class,
+        ReadOnlyPropertyRector::class,
+        MixedTypeRector::class,
+        
+        // Code quality
+        InlineConstructorDefaultToPropertyRector::class,
+        SimplifyIfReturnBoolRector::class,
+        SimplifyEmptyArrayCheckRector::class,
+        StaticClosureRector::class,
+        
+        // Dead code removal
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class,
+        RemoveUselessVarTagRector::class,
+        VarConstantCommentRector::class,
+    ])
+    ->withTypeCoverageLevel(80)
+    ->withDeadCodeLevel(30)
+    ->withCodeQualityLevel(30)
+    ->withImportNames()
+    ->withParallel(120, 8, 10);
